01e06e1a8642d0f676c9c90e35e833b38ee4a7c8
Taha <taha_altindal@hotmail.com>
2018-11-13 11:45:43 +0300

    Initial commit

---
+++ WindowsServicesToggle/WindowsServicesToggle/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Servislerr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerlog = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(79, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Windows Service:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(19, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(163, 74);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(254, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(398, 108);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(43, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Timer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(94, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 150);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(202, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerlog
+            // 
+            this.timerlog.Enabled = true;
+            this.timerlog.Interval = 1000;
+            this.timerlog.Tick += new System.EventHandler(this.timerlog_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(138, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(2, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(696, 179);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Otomatik";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(2, 189);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(696, 161);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "El ile";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(204, 19);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(186, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(410, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 26);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Çıkış";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(19, 47);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(371, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(123, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            this.backgroundWorker3.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker3_RunWorkerCompleted);
+            // 
+            // backgroundWorker4
+            // 
+            this.backgroundWorker4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker4_DoWork);
+            this.backgroundWorker4.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker4_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 353);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerlog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker4;
+    }
+}
+
---
+++ WindowsServicesToggle/WindowsServicesToggle/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Timers;
+
+namespace Servislerr
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.Visible = false;
+            GetWindowServices();
+            GetWindowServices();
+
+
+        }
+
+
+        private void GetWindowServices()
+        {
+            //throw new NotImplementedException();
+            ServiceController[] service;
+            service = ServiceController.GetServices();
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            for (int i = 0; i < service.Length; i++)
+            {
+                comboBox1.Items.Add(service[i].ServiceName);
+                comboBox2.Items.Add(service[i].ServiceName);
+            }
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //kaldığım yer           
+            try
+            {
+                string tarih = DateTime.Now.ToShortDateString();
+                string saat = DateTime.Now.ToLongTimeString();
+                //
+                var singleservice = new ServiceController((string)e.Argument);
+               // if ((singleservice.Status.Equals(ServiceControllerStatus.Stopped)) || (singleservice.Status.Equals(ServiceControllerStatus.StopPending)))
+                if ((singleservice.Status.Equals(ServiceControllerStatus.Running)) || (singleservice.Status.Equals(ServiceControllerStatus.Paused)))
+                {
+
+
+                    singleservice.Stop();
+                    listBox1.Items.Add(comboBox1.Text + " Servis durduruldu." + tarih + " " + saat + " \n ");
+                    Thread.Sleep(5000);
+                    singleservice.Start();
+                    listBox1.Items.Add(comboBox1.Text + " servisi tekrar başlatıldı !." + tarih + " " + saat + "\n");
+                }
+                if ((singleservice.Status.Equals(ServiceControllerStatus.Stopped)) || (singleservice.Status.Equals(ServiceControllerStatus.StopPending)))
+                {
+                    singleservice.Start();
+                    listBox1.Items.Add(comboBox1.Text + " Servis çalıştırıldı." + tarih + " " + saat + " \n ");
+                }
+                // timer1.Stop();
+            }
+
+            catch
+            {
+                MessageBox.Show("Bir hata oluştu.");
+            }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.progressBar1.Visible = false;
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.ToString(), "Servis Başlatılamadı.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Add("Program Başlatıldı." + "\n");//listbox'a log girildi
+            if (checkBox1.Checked == true)
+            {
+                label3.Text = ((int.Parse(textBox1.Text)) * 60).ToString();
+                timer1.Start();
+            }
+
+            this.progressBar1.Visible = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string tarih = DateTime.Now.ToShortDateString();
+                string saat = DateTime.Now.ToLongTimeString();
+                timer1.Stop();
+                this.backgroundWorker2.RunWorkerAsync(comboBox2.Text);
+                listBox1.Items.Add(comboBox1.Text + " Servis Durduruldu.  " + tarih + " " + saat + "\n");
+                progressBar1.Hide();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hata.");
+               
+            }
+           
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label4.Text = comboBox1.Text;
+            ServiceController singleservice = new ServiceController(label4.Text);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            /*label3.Text = ((int.Parse(textBox1.Text)) * 60).ToString();
+            this.backgroundWorker1.RunWorkerAsync(label4.Text);*/
+
+
+            label3.Text = ((int.Parse(label3.Text) - 1).ToString());
+            if (int.Parse(label3.Text) < 10)
+            {
+                label3.ForeColor = Color.Red;
+
+            }
+            else
+            {
+                label3.ForeColor = Color.Black;
+            }
+            if (int.Parse(label3.Text) < 1)
+            {
+                label3.Text = ((int.Parse(textBox1.Text)) * 60).ToString();
+                this.backgroundWorker1.RunWorkerAsync(label4.Text);
+                    
+
+
+
+            }
+
+        }
+
+        private void timerlog_Tick(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            FileStream fs = File.Create(path + "\\LOG.txt");
+            fs.Close();
+            String[] sitesDizi = new String[listBox1.Items.Count];
+            listBox1.Items.CopyTo(sitesDizi, 0);
+            System.IO.File.WriteAllLines(path + "\\LOG.txt", sitesDizi);
+        }
+
+        private void backgroundWorker2_DoWork_1(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                string tarih = DateTime.Now.ToShortDateString();
+                string saat = DateTime.Now.ToLongTimeString();
+                //
+                var singleservice = new ServiceController((string)e.Argument);
+                // if ((singleservice.Status.Equals(ServiceControllerStatus.Stopped)) || (singleservice.Status.Equals(ServiceControllerStatus.StopPending)))
+                if (singleservice.Status.Equals(ServiceControllerStatus.Running))
+                {
+
+
+                    singleservice.Stop();
+                    listBox1.Items.Add(comboBox1.Text + " Servis Durduruldu.  " + tarih + " " + saat + "\n");
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                //MessageBox.Show("Hata");
+            }
+            
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+           
+            try
+            {
+                string tarih = DateTime.Now.ToShortDateString();
+                string saat = DateTime.Now.ToLongTimeString();
+                //
+                var singleservice = new ServiceController((string)e.Argument);
+                // if ((singleservice.Status.Equals(ServiceControllerStatus.Stopped)) || (singleservice.Status.Equals(ServiceControllerStatus.StopPending)))
+                if (singleservice.Status.Equals(ServiceControllerStatus.Stopped))
+                {
+
+
+                    singleservice.Start();
+                    //Thread.Sleep(5000);
+                    listBox2.Items.Add(comboBox1.Text + " Servis Çalıştırıldı.  " + tarih + " " + saat + "\n");
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+               MessageBox.Show("Hata");
+            }
+            
+        }
+
+        private void backgroundWorker3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.progressBar1.Visible = false;
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.ToString(), "Servis Başlatılamadı.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //this.button3.Enabled = false;
+            this.backgroundWorker3.RunWorkerAsync(comboBox2.Text);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ServiceController singleservice = new ServiceController(comboBox2.Text);
+            if ((singleservice.Status.Equals(ServiceControllerStatus.Stopped)) || (singleservice.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+
+              //  button4.Enabled = false;
+               // button3.Enabled = true;
+            }
+                else
+                {
+               // button4.Enabled = true;
+               // button3.Enabled = false;
+                }
+            
+            }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //this.button3.Enabled = false;
+            this.backgroundWorker4.RunWorkerAsync(comboBox2.Text);
+
+        }
+
+        private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            try
+            {
+                string tarih = DateTime.Now.ToShortDateString();
+                string saat = DateTime.Now.ToLongTimeString();
+                //
+                var singleservice = new ServiceController((string)e.Argument);
+                // if ((singleservice.Status.Equals(ServiceControllerStatus.Stopped)) || (singleservice.Status.Equals(ServiceControllerStatus.StopPending)))
+                if (singleservice.Status.Equals(ServiceControllerStatus.Running))
+                {
+
+
+                    singleservice.Stop();
+                    listBox2.Items.Add(comboBox1.Text + " Servis Durduruldu.  " + tarih + " " + saat + "\n");
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hata");
+            }
+            
+         
+        }
+
+
+
+        public int i { get; set; }
+
+        private void backgroundWorker4_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.progressBar1.Visible = false;
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.ToString(), "Servis Başlatılamadı.");
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+
+   
+        }
+    }
+
+    
